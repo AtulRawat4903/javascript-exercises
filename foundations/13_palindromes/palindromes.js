4f385b9b374d5afb71e7fa24c131ad4705b04195
Atul Rawat <atulrawat104903@gmail.com>
2026-06-27 00:25:26 +0530

    Finished the 13_palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lower = string.toLowerCase();
+    const replace = lower.replace(/[^a-z0-9]/g, "");
+    const reverse = replace.split("").reverse().join("");
 
+    return replace === reverse;
 };
 
 // Do not edit below this line
