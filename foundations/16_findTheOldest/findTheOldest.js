ec865641bec12a3d654fb5f71995d06d4832700b
Atul Rawat <atulrawat104903@gmail.com>
2026-06-27 01:08:05 +0530

    Finished the 16_findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+        const oldestDeathYear = oldest.yearOfDeath || new Date().getFullYear();
+        const currentDeathYear = current.yearOfDeath || new Date().getFullYear();
 
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+        const currentAge = currentDeathYear - current.yearOfBirth;
+
+        return currentAge > oldestAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
